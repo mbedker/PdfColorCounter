af518607d86973e0e22096700370ef81eadaf6cb
Matt Bedker <matthewjbedker@gmail.com>
2014-10-19 13:44:53 -0400

    Pre jekyll install for zeroclipboard testing.

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ colorCounterApp.config(['$routeProvider',
     function($routeProvider) {
         $routeProvider.
         when('/start', {
-            templateUrl: '/assets/partials/submitPdf.html',
+            templateUrl: '/assets/partials/submit-pdf.html',
             controller: 'SubmitPdfCtrl'
         }).
         when('/main', {
@@ -19,4 +19,22 @@ colorCounterApp.config(['$routeProvider',
         otherwise({
             redirectTo: '/start'
         });
-}]);
+}]).run( function($rootScope, $location) {
+          // register listener to watch route changes
+          $rootScope.$on( "$routeChangeStart", function(event, next, current) {
+              if (!window.pdfSession) {
+                  if (window.localStorage && window.localStorage.getItem("pdfSession")) {
+                      // Load previous session if we have it.
+                      window.pdfSession = JSON.parse(window.localStorage.getItem("pdfSession"));
+                  } else {
+                      // If we don't have a pdf session in memory we need to upload a pdf file
+                      if (next.originalPath == "/start") {
+                          // already going to #start, no redirect needed
+                      } else {
+                          // not going to #start, we should redirect now
+                          $location.path( "/start" );
+                      }
+                  }
+              }
+          });
+      });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -11,10 +11,10 @@ colorCounterControllers.controller('SubmitPdfCtrl', ['$scope', '$location', 'fil
             }
             if (d) {
                 //window.onbeforeunload = function(){} to handel before page unloads
-                $scope.uploadResult = d;
-                window.uploadResult = d;
-                if(window.sessionStorage) {
-                window.sessionStorage.setItem("uploadResult", d);
+                $scope.pdfSession = d;
+                window.pdfSession = d;
+                if (window.localStorage) {
+                    window.localStorage.setItem("pdfSession", JSON.stringify(d));
                 }
             $location.path ('/main');
             }
@@ -23,6 +23,8 @@ colorCounterControllers.controller('SubmitPdfCtrl', ['$scope', '$location', 'fil
 }]);
 
 colorCounterControllers.controller('MainViewCtrl', ['$scope', 'getStatus', '$http', function($scope, getStatus, $http) {
+        var table = $('#thumbnail-table');
+        var tbody = table.find('tbody');
     $scope.getPdfStatus = function(result, callback){
         getStatus.getPdfStatus(result, callback);
     };
@@ -30,30 +32,52 @@ colorCounterControllers.controller('MainViewCtrl', ['$scope', 'getStatus', '$htt
         var timerInterval = 1000,
             $this = this;
             filter = [];
-        var table = $('#thumbnail-table');
-        var tbody = table.find('tbody');
         var displayedPages = [];
+        window.selectedPages = [];
+        var colorPages = [];
+
+        $scope.pageColorPercentMap = {};
 
         var containerHtml = '<tr>';
-        for (var pageNumber = 1; pageNumber <= window.uploadResult.numberOfPages; pageNumber++){
-           containerHtml = containerHtml + '<td><div id="page' + pageNumber + '" class = "page-wrapper"></div></td>';
+        for (var pageNumber = 1; pageNumber <= window.pdfSession.numberOfPages; pageNumber++){
+           containerHtml = containerHtml + '<td><div class="page-wrapper"><div id="' + pageNumber + '" class="selectable unselected"></div></div></td>';
         }
         containerHtml = containerHtml + '</tr>';
         tbody.append(containerHtml);
         //add wrappers, inject content, when its done parsing
         //setting width and height of wrapper beforehand to have room for
         //tbody.
+
+        var onSelectPageFunction = function() {
+            var element = $(this);
+            element.toggleClass('selected');
+            element.toggleClass('unselected')
+
+            var pageNum = element.attr('id');
+            if (element.hasClass('selected')) {
+                window.selectedPages.push(pageNum);
+            } else {
+                window.selectedPages.splice( $.inArray(pageNum, window.selectedPages), 1 );
+            }
+
+            console.log(window.selectedPages); // To be removed
+        }
+
         var timer = window.setInterval(function(){
-            $this.getPdfStatus(window.uploadResult, function(data){
+            $this.getPdfStatus(window.pdfSession, function(data){
                 if (data) {
+                window.pdfStatus = data;
                     for (var i = 0; i < data.completedPages.length; i++) {
                         var pg = data.completedPages[i];
 
                         if (filter.indexOf(pg.pageNumber)<=-1) {
-                            console.log('Append: ' + pg.pageNumber);
-                            var pageContainer = tbody.find('#page'+ pg.pageNumber);
-                            pageContainer.append('<img src = "/pdf/thumbnail/' + data.sessionId + '/' + pg.pageNumber + '"></img><div>' + pg.pageNumber + " : " + pg.percentColor + '</div>');
+                            var pageContainer = tbody.find('#'+ pg.pageNumber);
+                            pageContainer.append('<img src="/pdf/thumbnail/' + data.sessionId + '/' + pg.pageNumber + '"></img><div>' + pg.pageNumber + " : " + pg.percentColor + '</div>');
+                            pageContainer.click(onSelectPageFunction);
+                            pageContainer.onChange
                             filter.push(pg.pageNumber);
+                            $scope.pageColorPercentMap[pg.pageNumber] = pg.percentColor;
+
                         }
                     }
                     if (data.isComplete)
@@ -67,4 +91,86 @@ colorCounterControllers.controller('MainViewCtrl', ['$scope', 'getStatus', '$htt
                 return timerInterval;
                 }(timerInterval)));
     };
+
+        ZeroClipboard.config( {moviePath: 'assets/zeroClipbaord/ZeroClipboard.swf'});
+        var client = new ZeroClipboard($('#bw-print-set'));
+
+        client.on( 'load', function(client) {
+
+          client.on( 'datarequested', function(client, text) {
+            var text = $scope.copyBWPages;
+            client.setText(text);
+            console.log(text);
+          });
+
+          // callback triggered on successful copying
+          client.on( 'complete', function(client, args) {
+            console.log("Text copied to clipboard: \n" + args.text );
+          });
+        });
+
+        // In case of error - such as Flash not being available
+        client.on( 'wrongflash noflash', function() {
+          ZeroClipboard.destroy();
+        } );
+
+
+
+        $scope.copyBWPages = function(){
+        var bwPages = [];
+//           var table = $('#thumbnail-table');
+//         var tbody = table.find('tbody');
+            for (i = 0; i < window.pdfSession.numberOfPages; i++){
+                var page = tbody.find('#' + i);
+                if (page.hasClass('unselected')) {
+                    bwPages.push(i);
+                }
+        }
+        console.log(bwPages);
+        }
+
+        $(function(){
+            $("#slider-range").slider({
+            range: true,
+            min: 0,
+            max: 100,
+            values: [ 0, 100 ],
+            orientation: 'vertical',
+            slide: function( event, ui ) {
+                $( "#amount" ).val( ui.values[ 0 ] + "% - " + ui.values[ 1 ] + "%" );
+                },
+            change: function (event, ui) {
+                var upperRange = ui.values[1];
+                var lowerRange = ui.values[0];
+                console.log( upperRange + ':' + lowerRange);
+
+                if (!window.pdfStatus)
+                    return;
+
+                //var tbody = $('#thumbnail-table tbody');
+                for (var i = 0; i < window.pdfStatus.completedPages.length; i++) {
+                    var pg = window.pdfStatus.completedPages[i];
+                    var element = tbody.find('#' + pg.pageNumber);
+
+                    if (pg.percentColor > upperRange) {
+                        console.log('hide ' + pg.pageNumber);
+                        element.parent().parent().hide();
+                        window.selectedPages.push(pg.pageNumber);
+                    } else if (pg.percentColor < lowerRange) {
+                        console.log('hide ' + pg.pageNumber);
+                        element.parent().parent().hide();
+                        window.selectedPages.splice( $.inArray(pg.pageNumber, window.selectedPages), 1 );
+                    } else {
+                        console.log('show ' + pg.pageNumber);
+                        element.removeClass('selected');
+                        window.selectedPages.splice( $.inArray(pg.pageNumber, window.selectedPages), 1 );
+                        element.parent().parent().show();
+                    }
+                }
+            }
+            });
+            $( "#amount" ).val(  $( "#slider-range" ).slider( "values", 0 ) +
+                "% - " + $( "#slider-range" ).slider( "values", 1 ) + "%" );
+        });
+
 }]);
